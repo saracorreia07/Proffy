fbe1f147be74a2424ab23a5d235887721bda12b7
Sara Correia <sara.dsc@hotmail.com>
2020-09-01 17:45:29 +0100

    finished Landing page frontend

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+
+import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
+
+function Landing() {
+    return (
+        <View style={styles.container}>
+            <Image source={landingImg} style={styles.banner} />
+            <Text style={styles.title}>
+                Welcome, {'\n'}
+                <Text style={styles.titleBold}>
+                    What do you want to do?
+                </Text>
+            </Text>
+
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                    <Image source={studyIcon} />
+                    <Text style={styles.buttonText}>Study</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                    <Image source={giveClassesIcon} />
+                    <Text style={styles.buttonText}>Teach</Text>
+                </TouchableOpacity>
+            </View>
+
+            <Text style={styles.totalConnections}>
+                Total of 285 connections made {'  '}
+                <Image source={heartIcon} />
+            </Text>
+        </View>
+    );
+}
+
+export default Landing;
