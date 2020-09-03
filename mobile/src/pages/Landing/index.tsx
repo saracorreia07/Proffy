19a5cf8cd46cecdf2aec5f05f0a59855383d1502
Sara Correia <sara.dsc@hotmail.com>
2020-09-03 15:14:23 +0100

    started naviagtion

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
 
@@ -9,6 +10,13 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
+
+    const { navigate } = useNavigation();
+
+    function handleNavigateToGiveClassesPage() {
+        navigate('GiveClasses');
+    }
+
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
@@ -25,7 +33,10 @@ function Landing() {
                     <Text style={styles.buttonText}>Study</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                <TouchableOpacity
+                    onPress={handleNavigateToGiveClassesPage}
+                    style={[styles.button, styles.buttonSecondary]}
+                >
                     <Image source={giveClassesIcon} />
                     <Text style={styles.buttonText}>Teach</Text>
                 </TouchableOpacity>
