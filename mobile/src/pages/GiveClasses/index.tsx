e065088176f12e9908bfc2637a7f5c7deeec9039
Sara Correia <sara.dsc@hotmail.com>
2020-09-03 19:36:48 +0100

    finished mobile frontend

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
 
 function GiveClasses() {
-    return <View />
+
+    const { goBack } = useNavigation();
+
+    function handleNavigateBack() {
+        goBack();
+    }
+
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                resizeMode="contain"
+                source={giveClassesBgImage}
+                style={styles.content}
+            >
+                <Text style={styles.title}>Do you want to become a Proffy?</Text>
+                <Text style={styles.description}>To start, you need to register as a teacher on our web platform.</Text>
+
+            </ImageBackground>
+
+            <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+                <Text style={styles.okButtonText}>Ok</Text>
+            </RectButton>
+        </View>
+    );
 }
 
 export default GiveClasses;
--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
@@ -17,6 +18,10 @@ function Landing() {
         navigate('GiveClasses');
     }
 
+    function handleNavigateToStudyPages() {
+        navigate('Study');
+    }
+
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
@@ -28,18 +33,21 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                <RectButton 
+                onPress={handleNavigateToStudyPages}
+                style={[styles.button, styles.buttonPrimary]}
+                >
                     <Image source={studyIcon} />
                     <Text style={styles.buttonText}>Study</Text>
-                </TouchableOpacity>
+                </RectButton>
 
-                <TouchableOpacity
+                <RectButton
                     onPress={handleNavigateToGiveClassesPage}
                     style={[styles.button, styles.buttonSecondary]}
                 >
                     <Image source={giveClassesIcon} />
                     <Text style={styles.buttonText}>Teach</Text>
-                </TouchableOpacity>
+                </RectButton>
             </View>
 
             <Text style={styles.totalConnections}>
